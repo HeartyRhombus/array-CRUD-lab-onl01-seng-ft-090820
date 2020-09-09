e6663cbc8b0b164fa0d0abf8424f22153f8cd6dc
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-09 18:16:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,8 +11,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  add_to_start = ["surprise", "excited"]
-  add_to_start.unshift("wow")
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
